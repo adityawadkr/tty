ba9e2c851ef4048ee408d0aa1f76aafda684ef98
Aditya Wadkar <105574366+adityawadkr@users.noreply.github.com>
2025-11-18 11:38:28 +0530

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,33 @@
 import { FlatCompat } from '@eslint/eslintrc'
- 
+import tseslint from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+
 const compat = new FlatCompat({
-  // import.meta.dirname is available after Node.js v20.11.0
   baseDirectory: import.meta.dirname,
 })
- 
-const eslintConfig = [
+
+export default [
   ...compat.config({
     extends: ['next'],
     plugins: ['import'],
   }),
+
   {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
     rules: {
       'react/no-unescaped-entities': 'off',
       '@next/next/no-img-element': 'off',
+
+      // TypeScript rules
       '@typescript-eslint/no-unused-vars': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
+
       'react-hooks/exhaustive-deps': 'off',
       'import/no-unresolved': 'error',
       'import/named': 'error',
@@ -29,5 +41,3 @@ const eslintConfig = [
     },
   },
 ]
- 
-export default eslintConfig
